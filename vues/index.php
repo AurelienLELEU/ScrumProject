fa8dc474db2ade74fa7426d6985822d142093ccb
AurelienLELEU <aurelien.leleu78@gmail.com>
2023-11-03 16:52:02 +0100

    CASHCASH

--- vues/index.php
+++ vues/index.php
@@ -315,41 +315,42 @@ if ($_POST) { // creation du commentaire
                                         <?php } ?>
                                     </form>
                                 </div>
-                                <?php foreach ($commentairesManager->getAll() as $commentaire) {
-                                    if ($commentaire->getId_post() == $post->getId()) { ?>
-                                        <div class="d-flex">
-                                            <div class="card-body">
-                                                <p class="card-text my-0"> <b><?php echo ($commentaire->getUser_name() . ' ') ?></b><?= $commentaire->getContent() ?></p>
-                                                <small class="text-muted"><?= $commentaire->getCreated_at() ?></small>
-                                            </div>
-                                            <div class="p-3">
-                                                <?php if (isset($_SESSION["username"])) {
-                                                    if ($_SESSION["username"] == $post->getUser_name() || $_SESSION["username"] == $commentaire->getUser_name()) { ?>
-                                                        <a class="dropdown-item" href="delete_comment.php?id=<?= $commentaire->getId() ?>">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                                                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                                                            </svg>
-                                                        </a>
+                                <p class="mb-1 card-text"> <?= $likesManager->countRelated($post->getId())[0][0] ?> Avis fiable, <?= $dislikesManager->countRelated($post->getId())[0][0] ?> Avis pas fiable</p>
+                                <div class="card-footer">
+                                    <?php foreach ($commentairesManager->getAll() as $commentaire) {
+                                        if ($commentaire->getId_post() == $post->getId()) { ?>
+                                            <div class="d-flex">
+                                                <div class="card-body">
+                                                    <p class="card-text my-0"> <b><?php echo ($commentaire->getUser_name() . ' ') ?></b><?= $commentaire->getContent() ?></p>
+                                                    <small class="text-muted"><?= $commentaire->getCreated_at() ?></small>
+                                                </div>
+                                                <div class="p-3">
+                                                    <?php if (isset($_SESSION["username"])) {
+                                                        if ($_SESSION["username"] == $post->getUser_name() || $_SESSION["username"] == $commentaire->getUser_name()) { ?>
+                                                            <a class="dropdown-item" href="delete_comment.php?id=<?= $commentaire->getId() ?>">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                                                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                                                </svg>
+                                                            </a>
 
-                                                <?php }
-                                                } ?>
+                                                    <?php }
+                                                    } ?>
+                                                </div>
                                             </div>
-                                        </div>
-                                <?php }
-                                } ?>
-                                <form method="post" class="bg-white card-footer">
-                                    <div class="content">
-                                        <div class="col-auto">
-                                            <input class="form-control" name="content" id="content" placeholder="Ajouter un commentaire...">
-                                            <input type="hidden" name="post_id" id="post_id" value="<?php echo ($post->getId()); ?>">
-                                            <input type="hidden" name="type" id="type" value="com">
-                                        </div>
-                                    </div>
-                                </form>
+                                    <?php }
+                                    } ?>
+                                </div>
                         <?php }
                         } ?>
-                        <p class="mb-1 card-text"> <?= $likesManager->countRelated($post->getId())[0][0] ?> Avis fiable, <?= $dislikesManager->countRelated($post->getId())[0][0] ?> Avis pas fiable</p>
-
+                        <form method="post" class="bg-white card-footer">
+                            <div class="content">
+                                <div class="col-auto">
+                                    <input class="form-control" name="content" id="content" placeholder="Ajouter un commentaire...">
+                                    <input type="hidden" name="post_id" id="post_id" value="<?php echo ($post->getId()); ?>">
+                                    <input type="hidden" name="type" id="type" value="com">
+                                </div>
+                            </div>
+                        </form>
                         <small class="text-muted"> <?= $post->getCreated_at() ?> </small>
                     </div>
                 </div>
